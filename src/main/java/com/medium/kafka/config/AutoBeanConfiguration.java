da786d61b455cfc3ef22395853d969060e3bc9e6
tauseef-ameen <tauseef.125002@gmail.com>
2025-02-06 20:25:41 +0100

    Added logging while creating topic

--- src/main/java/com/medium/kafka/config/AutoBeanConfiguration.java
+++ src/main/java/com/medium/kafka/config/AutoBeanConfiguration.java
@@ -1,5 +1,6 @@
 package com.medium.kafka.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@Slf4j
 public class AutoBeanConfiguration {
 
     @Value("${spring.topic.name}")
@@ -14,10 +16,13 @@ public class AutoBeanConfiguration {
 
     @Bean
     public NewTopic createTopic() {
+        log.info("Trying to create topic with name: {}", topicName);
 
-        return TopicBuilder.name(topicName)
+        NewTopic newTopic = TopicBuilder.name(topicName)
                 .partitions(3)
                 .replicas(3)
                 .build();
+        log.info("topic created with name: {}", newTopic.name());
+        return newTopic;
     }
 }
