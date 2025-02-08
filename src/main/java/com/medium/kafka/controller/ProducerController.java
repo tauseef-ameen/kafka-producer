a34c1a687c7d37737d33aa987cd8f03aabb38749
tauseef-ameen <tauseef.125002@gmail.com>
2025-02-08 11:44:11 +0100

    Refactored Book to Order

--- src/main/java/com/medium/kafka/controller/ProducerController.java
+++ src/main/java/com/medium/kafka/controller/ProducerController.java
@@ -1,13 +1,14 @@
 package com.medium.kafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.medium.kafka.dto.BookCatalogEvent;
-import com.medium.kafka.dto.EventType;
+import com.medium.kafka.dto.OrderEvent;
+import com.medium.kafka.dto.OrderType;
 import com.medium.kafka.service.KafkaProducerService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,55 +26,55 @@ public class ProducerController {
         this.kafkaProducerService = kafkaProducerService;
     }
 
-    @PostMapping("/v1/book")
+    @PostMapping("/v1/order")
     @Operation(summary="produce kafka message in asynchronous way", description="Approach 1 to send kafka message")
-    public ResponseEntity<BookCatalogEvent> postBookCatalogEvent
-            (@RequestBody BookCatalogEvent bookCatalogEvent) throws JsonProcessingException {
-        log.info("sendBookCatalogEvent via approach1: {}", bookCatalogEvent);
-        kafkaProducerService.sendBookEvent_Approach1(bookCatalogEvent);
-        log.info("Message sent successfully via /v1/book api");
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookCatalogEvent);
+    public ResponseEntity<OrderEvent> postOrderEvent
+            (@RequestBody OrderEvent orderEvent) throws JsonProcessingException {
+        log.info("send order details asynchronously using key value serializer and completable future: {}", orderEvent);
+        kafkaProducerService.sendOrderEventApproach1(orderEvent);
+        log.info("Message sent successfully using /v1/order api");
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderEvent);
 
     }
 
-    @PostMapping("/v2/book")
+    @PostMapping("/v2/order")
     @Operation(summary="produce kafka message in synchronous way", description="Approach 2 to send kafka message")
-    public ResponseEntity<BookCatalogEvent> postBookCatalogEvent2(@RequestBody BookCatalogEvent bookCatalogEvent)
+    public ResponseEntity<OrderEvent> postOrderEvent2(@RequestBody OrderEvent orderEvent)
             throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
-        log.info("sendBookCatalogEvent via approach2: {}", bookCatalogEvent);
-        kafkaProducerService.sendBookEvent_Approach2(bookCatalogEvent);
-        log.info("Message sent successfully via /v2/book api");
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookCatalogEvent);
+        log.info("send order details synchronously: {}", orderEvent);
+        SendResult<Integer, String> result = kafkaProducerService.sendOrderEventApproach2(orderEvent);
+        log.info("Message sent using /v2/order api to topic : {}", result.getRecordMetadata().topic());
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderEvent);
 
     }
 
-    @PostMapping("/v3/book")
+    @PostMapping("/v3/order")
     @Operation(summary="produce kafka message in asynchronous way using Producer Record", description="Approach 3 to send kafka message")
-    public ResponseEntity<BookCatalogEvent> postBookCatalogEvent3(@RequestBody BookCatalogEvent bookCatalogEvent) throws JsonProcessingException {
-        log.info("sendBookCatalogEvent via approach3: {}", bookCatalogEvent);
-        kafkaProducerService.sendBookEvent_Approach3(bookCatalogEvent);
-        log.info("Message sent successfully via /v3/book api");
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookCatalogEvent);
+    public ResponseEntity<OrderEvent> postOrderEvent3(@RequestBody OrderEvent orderEvent) throws JsonProcessingException {
+        log.info("send order details asynchronously using producer record: {}", orderEvent);
+        kafkaProducerService.sendOrderEventApproach3(orderEvent);
+        log.info("Message sent successfully using /v3/order api");
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderEvent);
 
     }
 
-    @PutMapping("/v1/book")
+    @PutMapping("/v1/order")
     @Operation(summary="produce kafka message using valid key and event type", description="send kafka message using PUT request")
-    public ResponseEntity<?> putBookCatalogEvent(@RequestBody BookCatalogEvent bookCatalogEvent) throws JsonProcessingException {
-        log.info("validating book event via put endpoint: {}", bookCatalogEvent);
-        final ResponseEntity<String> response = validateBookEvent(bookCatalogEvent);
+    public ResponseEntity<?> putOrderEvent(@RequestBody OrderEvent orderEvent) throws JsonProcessingException {
+        log.info("validating order event via put endpoint: {}", orderEvent);
+        final ResponseEntity<String> response = validateOrderEvent(orderEvent);
         if (response != null) {
             return response;
         }
-        kafkaProducerService.sendBookEvent_Approach3(bookCatalogEvent);
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookCatalogEvent);
+        kafkaProducerService.sendOrderEventApproach3(orderEvent);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderEvent);
     }
 
-    private static ResponseEntity<String> validateBookEvent(BookCatalogEvent bookCatalogEvent) {
-        if (bookCatalogEvent.eventId() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please provide event id");
+    private static ResponseEntity<String> validateOrderEvent(OrderEvent orderEvent) {
+        if (orderEvent.orderId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please provide order id");
         }
-        if (!bookCatalogEvent.eventType().equals(EventType.UPDATE)) {
+        if (!orderEvent.orderType().equals(OrderType.UPDATE)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please provide event Type as update");
         }
         return null;
